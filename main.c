d4b074a954a9e3fd082203d5a6de7009b7a95fa0
Mosolov Anton <mosolov2000@gmail.com>
2025-02-26 16:20:25 +0000

    Fix task 1

--- main.c
+++ main.c
@@ -80,38 +80,43 @@
  #include "nrf_log_default_backends.h"
  #include "nrf_log_backend_usb.h"
  
- #define DEVICE_NAME "ESTC"                      /**< Name of device. Will be included in the advertising data. */
- #define MANUFACTURER_NAME "NordicSemiconductor" /**< Manufacturer. Will be passed to Device Information Service. */
+ 
+ #define DEVICE_NAME                     "ESTC"                                  /**< Name of device. Will be included in the advertising data. */
+ #define MANUFACTURER_NAME               "NordicSemiconductor"    
  #define USER_NAME "Anton"
- #define FULL_USER_NAME "Anton Mosolov"
- #define APP_ADV_INTERVAL 300 /**< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
+ #define FULL_USER_NAME "Anton Mosolov"               
+ #define APP_ADV_INTERVAL                300                                     /**< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
  
- #define APP_ADV_DURATION 18000  /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
- #define APP_BLE_OBSERVER_PRIO 3 /**< Application's BLE observer priority. You shouldn't need to modify this value. */
- #define APP_BLE_CONN_CFG_TAG 1  /**< A tag identifying the SoftDevice BLE configuration. */
+ #define APP_ADV_DURATION                18000                                   /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
+ #define APP_BLE_OBSERVER_PRIO           3                                       /**< Application's BLE observer priority. You shouldn't need to modify this value. */
+ #define APP_BLE_CONN_CFG_TAG            1                                       /**< A tag identifying the SoftDevice BLE configuration. */
  
- #define MIN_CONN_INTERVAL MSEC_TO_UNITS(100, UNIT_1_25_MS) /**< Minimum acceptable connection interval (0.1 seconds). */
- #define MAX_CONN_INTERVAL MSEC_TO_UNITS(200, UNIT_1_25_MS) /**< Maximum acceptable connection interval (0.2 second). */
- #define SLAVE_LATENCY 0                                    /**< Slave latency. */
- #define CONN_SUP_TIMEOUT MSEC_TO_UNITS(4000, UNIT_10_MS)   /**< Connection supervisory timeout (4 seconds). */
+ #define MIN_CONN_INTERVAL               MSEC_TO_UNITS(100, UNIT_1_25_MS)        /**< Minimum acceptable connection interval (0.1 seconds). */
+ #define MAX_CONN_INTERVAL               MSEC_TO_UNITS(200, UNIT_1_25_MS)        /**< Maximum acceptable connection interval (0.2 second). */
+ #define SLAVE_LATENCY                   0                                       /**< Slave latency. */
+ #define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)         /**< Connection supervisory timeout (4 seconds). */
  
- #define FIRST_CONN_PARAMS_UPDATE_DELAY APP_TIMER_TICKS(5000) /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
- #define NEXT_CONN_PARAMS_UPDATE_DELAY APP_TIMER_TICKS(30000) /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
- #define MAX_CONN_PARAMS_UPDATE_COUNT 3                       /**< Number of attempts before giving up the connection parameter negotiation. */
+ #define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(5000)                   /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
+ #define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(30000)                  /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
+ #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                       /**< Number of attempts before giving up the connection parameter negotiation. */
  
- #define DEAD_BEEF 0xDEADBEEF /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
+ #define DEAD_BEEF                       0xDEADBEEF                              /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
  
- NRF_BLE_GATT_DEF(m_gatt);           /**< GATT module instance. */
- NRF_BLE_QWR_DEF(m_qwr);             /**< Context for the Queued Write module.*/
- BLE_ADVERTISING_DEF(m_advertising); /**< Advertising module instance. */
  
- static uint16_t m_conn_handle = BLE_CONN_HANDLE_INVALID; /**< Handle of the current connection. */
+ NRF_BLE_GATT_DEF(m_gatt);                                                       /**< GATT module instance. */
+ NRF_BLE_QWR_DEF(m_qwr);                                                         /**< Context for the Queued Write module.*/
+ BLE_ADVERTISING_DEF(m_advertising);                                             /**< Advertising module instance. */
  
- static ble_uuid_t m_adv_uuids[] = /**< Universally unique service identifiers. */
-     {
-         {BLE_UUID_DEVICE_INFORMATION_SERVICE, BLE_UUID_TYPE_BLE}};
+ static uint16_t m_conn_handle = BLE_CONN_HANDLE_INVALID;                        /**< Handle of the current connection. */
+ 
+ static ble_uuid_t m_adv_uuids[] =                                               /**< Universally unique service identifiers. */
+ {
+     {BLE_UUID_DEVICE_INFORMATION_SERVICE, BLE_UUID_TYPE_BLE}
+ };
+ 
  
  static void advertising_start(void);
+ 
  
  /**@brief Callback function for asserts in the SoftDevice.
   *
@@ -124,7 +129,7 @@
   * @param[in] line_num   Line number of the failing ASSERT call.
   * @param[in] file_name  File name of the failing ASSERT call.
   */
- void assert_nrf_callback(uint16_t line_num, const uint8_t *p_file_name)
+ void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
  {
      app_error_handler(DEAD_BEEF, line_num, p_file_name);
  }
@@ -140,6 +145,7 @@
      APP_ERROR_CHECK(err_code);
  }
  
+ 
  /**@brief Function for the GAP initialization.
   *
   * @details This function sets up all the necessary GAP (Generic Access Profile) parameters of the
@@ -147,8 +153,8 @@
   */
  static void gap_params_init(void)
  {
-     ret_code_t err_code;
-     ble_gap_conn_params_t gap_conn_params;
+     ret_code_t              err_code;
+     ble_gap_conn_params_t   gap_conn_params;
      ble_gap_conn_sec_mode_t sec_mode;
  
      BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
@@ -165,12 +171,13 @@
  
      gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
      gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
-     gap_conn_params.slave_latency = SLAVE_LATENCY;
-     gap_conn_params.conn_sup_timeout = CONN_SUP_TIMEOUT;
+     gap_conn_params.slave_latency     = SLAVE_LATENCY;
+     gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
  
      err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
      APP_ERROR_CHECK(err_code);
  }
+ 
  
  /**@brief Function for initializing the GATT module.
   */
@@ -179,6 +186,7 @@
      ret_code_t err_code = nrf_ble_gatt_init(&m_gatt, NULL);
      APP_ERROR_CHECK(err_code);
  }
+ 
  
  /**@brief Function for handling Queued Write Module errors.
   *
@@ -196,7 +204,7 @@
   */
  static void services_init(void)
  {
-     ret_code_t err_code;
+     ret_code_t         err_code;
      nrf_ble_qwr_init_t qwr_init = {0};
  
      // Initialize Queued Write Module.
@@ -204,7 +212,9 @@
  
      err_code = nrf_ble_qwr_init(&m_qwr, &qwr_init);
      APP_ERROR_CHECK(err_code);
+ 
  }
+ 
  
  /**@brief Function for handling the Connection Parameters Module.
   *
@@ -216,7 +226,7 @@
   *
   * @param[in] p_evt  Event received from the Connection Parameters Module.
   */
- static void on_conn_params_evt(ble_conn_params_evt_t *p_evt)
+ static void on_conn_params_evt(ble_conn_params_evt_t * p_evt)
  {
      ret_code_t err_code;
  
@@ -227,6 +237,7 @@
      }
  }
  
+ 
  /**@brief Function for handling a Connection Parameters error.
   *
   * @param[in] nrf_error  Error code containing information about what went wrong.
@@ -236,27 +247,29 @@
      APP_ERROR_HANDLER(nrf_error);
  }
  
+ 
  /**@brief Function for initializing the Connection Parameters module.
   */
  static void conn_params_init(void)
  {
-     ret_code_t err_code;
+     ret_code_t             err_code;
      ble_conn_params_init_t cp_init;
  
      memset(&cp_init, 0, sizeof(cp_init));
  
-     cp_init.p_conn_params = NULL;
+     cp_init.p_conn_params                  = NULL;
      cp_init.first_conn_params_update_delay = FIRST_CONN_PARAMS_UPDATE_DELAY;
-     cp_init.next_conn_params_update_delay = NEXT_CONN_PARAMS_UPDATE_DELAY;
-     cp_init.max_conn_params_update_count = MAX_CONN_PARAMS_UPDATE_COUNT;
-     cp_init.start_on_notify_cccd_handle = BLE_GATT_HANDLE_INVALID;
-     cp_init.disconnect_on_fail = false;
-     cp_init.evt_handler = on_conn_params_evt;
-     cp_init.error_handler = conn_params_error_handler;
+     cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
+     cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
+     cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
+     cp_init.disconnect_on_fail             = false;
+     cp_init.evt_handler                    = on_conn_params_evt;
+     cp_init.error_handler                  = conn_params_error_handler;
  
      err_code = ble_conn_params_init(&cp_init);
      APP_ERROR_CHECK(err_code);
  }
+ 
  
  /**@brief Function for starting timers.
   */
@@ -266,7 +279,9 @@
         ret_code_t err_code;
         err_code = app_timer_start(m_app_timer_id, TIMER_INTERVAL, NULL);
         APP_ERROR_CHECK(err_code); */
+ 
  }
+ 
  
  /**@brief Function for handling advertising events.
   *
@@ -280,76 +295,77 @@
  
      switch (ble_adv_evt)
      {
-     case BLE_ADV_EVT_FAST:
-         NRF_LOG_INFO("Fast advertising.");
-         err_code = bsp_indication_set(BSP_INDICATE_ADVERTISING);
-         APP_ERROR_CHECK(err_code);
-         break;
+         case BLE_ADV_EVT_FAST:
+             NRF_LOG_INFO("Fast advertising.");
+             err_code = bsp_indication_set(BSP_INDICATE_ADVERTISING);
+             APP_ERROR_CHECK(err_code);
+             break;
  
-     default:
-         break;
+         default:
+             break;
      }
  }
+ 
  
  /**@brief Function for handling BLE events.
   *
   * @param[in]   p_ble_evt   Bluetooth stack event.
   * @param[in]   p_context   Unused.
   */
- static void ble_evt_handler(ble_evt_t const *p_ble_evt, void *p_context)
+ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
  {
      ret_code_t err_code = NRF_SUCCESS;
  
      switch (p_ble_evt->header.evt_id)
      {
-     case BLE_GAP_EVT_DISCONNECTED:
-         NRF_LOG_INFO("Disconnected.");
-         // LED indication will be changed when advertising starts.
-         break;
+         case BLE_GAP_EVT_DISCONNECTED:
+             NRF_LOG_INFO("Disconnected.");
+             // LED indication will be changed when advertising starts.
+             break;
  
-     case BLE_GAP_EVT_CONNECTED:
-         NRF_LOG_INFO("Connected.");
-         err_code = bsp_indication_set(BSP_INDICATE_CONNECTED);
-         APP_ERROR_CHECK(err_code);
-         m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
-         err_code = nrf_ble_qwr_conn_handle_assign(&m_qwr, m_conn_handle);
-         APP_ERROR_CHECK(err_code);
-         break;
+         case BLE_GAP_EVT_CONNECTED:
+             NRF_LOG_INFO("Connected.");
+             err_code = bsp_indication_set(BSP_INDICATE_CONNECTED);
+             APP_ERROR_CHECK(err_code);
+             m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+             err_code = nrf_ble_qwr_conn_handle_assign(&m_qwr, m_conn_handle);
+             APP_ERROR_CHECK(err_code);
+             break;
  
-     case BLE_GAP_EVT_PHY_UPDATE_REQUEST:
-     {
-         NRF_LOG_DEBUG("PHY update request.");
-         ble_gap_phys_t const phys =
+         case BLE_GAP_EVT_PHY_UPDATE_REQUEST:
+         {
+             NRF_LOG_DEBUG("PHY update request.");
+             ble_gap_phys_t const phys =
              {
                  .rx_phys = BLE_GAP_PHY_AUTO,
                  .tx_phys = BLE_GAP_PHY_AUTO,
              };
-         err_code = sd_ble_gap_phy_update(p_ble_evt->evt.gap_evt.conn_handle, &phys);
-         APP_ERROR_CHECK(err_code);
-     }
-     break;
+             err_code = sd_ble_gap_phy_update(p_ble_evt->evt.gap_evt.conn_handle, &phys);
+             APP_ERROR_CHECK(err_code);
+         } break;
  
-     case BLE_GATTC_EVT_TIMEOUT:
-         // Disconnect on GATT Client timeout event.
-         NRF_LOG_DEBUG("GATT Client Timeout.");
-         err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gattc_evt.conn_handle,
-                                          BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
-         APP_ERROR_CHECK(err_code);
-         break;
+         case BLE_GATTC_EVT_TIMEOUT:
+             // Disconnect on GATT Client timeout event.
+             NRF_LOG_DEBUG("GATT Client Timeout.");
+             err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gattc_evt.conn_handle,
+                                              BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+             APP_ERROR_CHECK(err_code);
+             break;
  
-     case BLE_GATTS_EVT_TIMEOUT:
-         // Disconnect on GATT Server timeout event.
-         NRF_LOG_DEBUG("GATT Server Timeout.");
-         err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gatts_evt.conn_handle,
-                                          BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
-         APP_ERROR_CHECK(err_code);
-         break;
+         case BLE_GATTS_EVT_TIMEOUT:
+             // Disconnect on GATT Server timeout event.
+             NRF_LOG_DEBUG("GATT Server Timeout.");
+             err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gatts_evt.conn_handle,
+                                              BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+             APP_ERROR_CHECK(err_code);
+             break;
  
-     default:
-         // No implementation needed.
-         break;
+         default:
+             // No implementation needed.
+             break;
      }
  }
+ 
  
  /**@brief Function for initializing the BLE stack.
   *
@@ -376,6 +392,7 @@
      NRF_SDH_BLE_OBSERVER(m_ble_observer, APP_BLE_OBSERVER_PRIO, ble_evt_handler, NULL);
  }
  
+ 
  /**@brief Function for handling events from the BSP module.
   *
   * @param[in]   event   Event generated when button is pressed.
@@ -386,68 +403,67 @@
  
      switch (event)
      {
-     case BSP_EVENT_DISCONNECT:
-         err_code = sd_ble_gap_disconnect(m_conn_handle,
-                                          BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
-         if (err_code != NRF_ERROR_INVALID_STATE)
-         {
-             APP_ERROR_CHECK(err_code);
-         }
-         break; // BSP_EVENT_DISCONNECT
-     default:
-         break;
+         case BSP_EVENT_DISCONNECT:
+             err_code = sd_ble_gap_disconnect(m_conn_handle,
+                                              BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+             if (err_code != NRF_ERROR_INVALID_STATE)
+             {
+                 APP_ERROR_CHECK(err_code);
+             }
+             break; // BSP_EVENT_DISCONNECT
+         default:
+             break;
      }
  }
+ 
  
  /**@brief Function for initializing the Advertising functionality.
   */
  static void advertising_init(void)
  {
-     ret_code_t err_code;
+     ret_code_t             err_code;
      ble_advertising_init_t init;
-     // Структура для вставки имени в  Advertising пакет через служебное поле информации
- 
+     
      ble_advdata_manuf_data_t short_name_payload;
      uint8_array_t short_name_to_payload;
-     // Структура для вставки полного имени в Scan Response пакет через служебное поле информации
+     
      ble_advdata_manuf_data_t full_name_payload;
      uint8_array_t full_name_to_payload;
- 
-     memset(&init, 0, sizeof(init));
- 
+     
      uint8_t user_name[] = USER_NAME;
-     short_name_to_payload.size = sizeof(user_name) / sizeof(user_name[0]);
+
+     
+     short_name_to_payload.size = sizeof(user_name);
      short_name_to_payload.p_data = user_name;
      short_name_payload.data = short_name_to_payload;
- 
+
      uint8_t full_user_name[] = FULL_USER_NAME;
-     full_name_to_payload.size = sizeof(full_user_name) / sizeof(full_user_name[0]);
+     full_name_to_payload.size = sizeof(full_user_name);
      full_name_to_payload.p_data = full_user_name;
      full_name_payload.data = full_name_to_payload;
      
-     // Иницализация пакета Advertising
-     init.advdata.name_type = BLE_ADVDATA_NO_NAME;
-     init.advdata.include_appearance = true;
-     init.advdata.flags = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
-     init.advdata.uuids_complete.uuid_cnt = sizeof(m_adv_uuids) / sizeof(m_adv_uuids[0]);
-     init.advdata.uuids_complete.p_uuids = m_adv_uuids;
-     init.advdata.p_manuf_specific_data = &short_name_payload;
-      
-     // Иницализация пакета Scan Response
-     init.srdata.name_type = BLE_ADVDATA_FULL_NAME;
-     init.srdata.include_appearance = true;
-     init.srdata.flags = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
-     init.srdata.uuids_complete.uuid_cnt = sizeof(m_adv_uuids) / sizeof(m_adv_uuids[0]);
-     init.srdata.uuids_complete.p_uuids = m_adv_uuids;
-     init.srdata.p_manuf_specific_data = &full_name_payload;
+
+     
  
-     init.config.ble_adv_fast_enabled = true;
+     memset(&init, 0, sizeof(init));
+ 
+     init.advdata.name_type               = BLE_ADVDATA_FULL_NAME;
+     init.advdata.include_appearance      = true;
+     init.advdata.flags                   = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
+     init.advdata.uuids_complete.uuid_cnt = sizeof(m_adv_uuids) / sizeof(m_adv_uuids[0]);
+     init.advdata.uuids_complete.p_uuids  = m_adv_uuids;
+     init.advdata.p_manuf_specific_data = &short_name_payload;
+
+     
+    
+ 
+     init.config.ble_adv_fast_enabled  = true;
      init.config.ble_adv_fast_interval = APP_ADV_INTERVAL;
-     init.config.ble_adv_fast_timeout = APP_ADV_DURATION;
+     init.config.ble_adv_fast_timeout  = APP_ADV_DURATION;
  
      // TODO: Add more data to the advertisement data
      // TODO: Add more data to the scan response data
- 
+     init.srdata.p_manuf_specific_data = &full_name_payload;
      init.evt_handler = on_adv_evt;
  
      err_code = ble_advertising_init(&m_advertising, &init);
@@ -455,6 +471,7 @@
  
      ble_advertising_conn_cfg_tag_set(&m_advertising, APP_BLE_CONN_CFG_TAG);
  }
+ 
  
  /**@brief Function for initializing buttons and leds.
   *
@@ -471,6 +488,7 @@
      APP_ERROR_CHECK(err_code);
  }
  
+ 
  /**@brief Function for initializing the nrf log module.
   */
  static void log_init(void)
@@ -481,6 +499,7 @@
      NRF_LOG_DEFAULT_BACKENDS_INIT();
  }
  
+ 
  /**@brief Function for initializing power management.
   */
  static void power_management_init(void)
@@ -489,6 +508,7 @@
      err_code = nrf_pwr_mgmt_init();
      APP_ERROR_CHECK(err_code);
  }
+ 
  
  /**@brief Function for handling the idle state (main loop).
   *
@@ -503,6 +523,7 @@
      LOG_BACKEND_USB_PROCESS();
  }
  
+ 
  /**@brief Function for starting advertising.
   */
  static void advertising_start(void)
@@ -510,6 +531,7 @@
      ret_code_t err_code = ble_advertising_start(&m_advertising, BLE_ADV_MODE_FAST);
      APP_ERROR_CHECK(err_code);
  }
+ 
  
  /**@brief Function for application main entry.
   */
@@ -539,6 +561,7 @@
          idle_state_handle();
      }
  }
+ 
  
  /**
   * @}
